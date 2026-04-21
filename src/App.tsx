6c3f682a9b62c6b24220052fd6bc07f8ebad6c2a
kaaalin <47788940+kaaalin@users.noreply.github.com>
2026-04-21 19:50:27 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1215,7 +1215,7 @@ function SquareView({
             justifyContent: "center",
             width: "100%",
             height: "100%",
-            transform: "translateY(2%)",
+            transform: "translateY(4%)",
             textShadow: piece.color === "white" ? "0 0 0.8px #000, 0 0 0.8px #000" : "none",
             WebkitTextStroke: piece.color === "white" ? "0.6px #000" : undefined,
             color: piece.color === "white" ? "#ffffff" : "#000000",
